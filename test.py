9469240d1d7a40547ce3eda59c30db40919c6b39
Rijo S Lal <slalrijo2005@gmail.com>
2024-12-13 20:32:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,27 @@
-# from tensorflow.keras.models import load_model
-# from tensorflow.keras.preprocessing.sequence import pad_sequences
-# import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
 
 
-# model = load_model('sentiment.h5')
+'''
+this part is to check whether the model works or not , not a part of sentio
+'''
+
+model = load_model('sentiment.h5')
 
 
-# with open('tokenizer.pkl', 'rb') as f:
-#     tokenizer = pickle.load(f)
+with open('tokenizer.pkl', 'rb') as f:
+    tokenizer = pickle.load(f)
 
-# text = ["the service were amazing"]
-
-
-# text_seq = tokenizer.texts_to_sequences(text)
-# text_padded = pad_sequences(text_seq, maxlen=200)
+text = ["the service were amazing"]
 
 
-# prediction = model.predict(text_padded)
+text_seq = tokenizer.texts_to_sequences(text)
+text_padded = pad_sequences(text_seq, maxlen=200)
 
 
-# print("Prediction shape:", prediction.shape)
-# print("Prediction values:", prediction)
-string="hello123 bro"
-print(string.lower())
+prediction = model.predict(text_padded)
+
+
+print("Prediction shape:", prediction.shape) #this should give 3 
+print("Prediction values:", prediction)
